77b53cdf0b7c62fdb0835132dc6f4ac765f9d2fa
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-09-21 17:59:27 +0530

    ::ui of sidebar::

--- src/lib/chat/leftSidebar.svelte
+++ src/lib/chat/leftSidebar.svelte
@@ -1,8 +1,19 @@
+<script>
+    let currentTab = 'Chat';
+
+    const showChats = () => currentTab = 'Chat';
+    const showCommunities = () => currentTab = 'Community';
+</script>
+
 <div class="ls-content">
     <div class="self-view">
         <img src="https://avatars.dicebear.com/api/pixel-art-neutral/:$spuckhafte.png" alt="avatar">
         <span class="ls-username">Carl Jhonson</span>
     </div>
     <div class="chat-community-menu"></div>
-    <div class="ls-controller"></div>
+    <div class="ls-controller">
+        <div class="chat" on:click={showChats}>Chats</div>
+        <div class="community" on:click={showCommunities}>Communities</div>
+        <div class="new-process">New {currentTab}</div>
+    </div>
 </div>
